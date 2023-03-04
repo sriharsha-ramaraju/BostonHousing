9164113429a4745d95a164032607277673ad90d8
John Doe <johndoe@example.com>
2023-03-04 01:32:55 +0000

    First commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='given the features such as crime rate, nitrous oxide concentration levels, avg num of rooms, age of property, the model should predict value of the property (median value of the property in $1000's)',
+    author='Dr Sriharsha Ramaraju',
+    license='',
+)
